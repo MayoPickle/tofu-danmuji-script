86647eb8c2cf43789ca36f2bfd1f1c0b3c45f507
Robert.Yu <me@boyangyu.cn>
2024-08-14 16:26:55 +0000

    fix: add room id schema

--- bili-live-script/main.py
+++ bili-live-script/main.py
@@ -6,7 +6,6 @@ from concurrent.futures import ThreadPoolExecutor, as_completed
 import argparse
 import inquirer
 
-# 全局配置变量
 CONFIG = {
     'ip_address': '127.0.0.1',
     'start_port': 23330,
@@ -30,14 +29,19 @@ def get_user_choice(room_data):
     questions = [inquirer.List('choice', message="Choose a room ID or add new one with remark", choices=choices)]
     answer = inquirer.prompt(questions)
     selected = answer['choice']
+    
     if selected == "Enter a new room ID with remark":
-        room_id = input("Enter new room ID: ")
-        remark = input("Enter remark for this room ID: ")
-        room_data.append({"room_id": room_id, "remark": remark})
-        save_room_data(room_data)
-        return room_id
+        while True:
+            room_id = input("Enter new room ID: ")
+            if room_id.isdigit(): 
+                remark = input("Enter remark for this room ID: ")
+                room_data.append({"room_id": int(room_id), "remark": remark})
+                save_room_data(room_data)
+                return int(room_id)
+            else:
+                print("Error: Room ID must be a number.")
     else:
-        return selected.split(" ")[0]
+        return int(selected.split(" ")[0])
 
 def get_fleet_nums():
     """获取用户想要调度的 fleet 数量列表。如果输入为0或为空，则返回所有 fleets。"""
